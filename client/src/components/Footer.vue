bc0fb2ea85a2af492696fb6c9df84535dd6a9179
Logan <75645441+Block354@users.noreply.github.com>
2024-05-09 09:52:50 +1200

    feat(client): clean-up code

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <div>Footer</div>
+</template>
+
+<style>
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <div>Navbar</div>
+</template>
+
+<style>
+</style>
