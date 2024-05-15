478261a55f4d22eb4dad4c3b5e8a303f6eb7cb5a
Logan <75645441+Block354@users.noreply.github.com>
2024-05-15 16:47:08 +1200

    Update Footer.vue

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -2,21 +2,8 @@
 </script>
 
 <template>
-    <footer class="bg-dark text-white pt-5 pb-5">
-        <div class="container text-center text-md-left">
-            <div class="row text-center text-md-left">
-
-                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Placeholder</h5>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam ipsum est distinctio perferendis
-                        dolores, officiis quod recusandae beatae iure? Dolor error corrupti iusto, nulla doloremque
-                        voluptate vitae ad veniam quia.</p>
-                </div>
-
-                
-            </div>
-        </div>
-    </footer>
+    <div>Footer Component</div>
 </template>
 
-<style></style>
+<style>
+</style>
