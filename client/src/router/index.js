0a3d7ca23f43fd61d591a7404ba759dfd4a81de9
Logan <75645441+Block354@users.noreply.github.com>
2024-05-15 19:23:32 +1200

    fix(client): clean-up code

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import LoginView from "@/views/account/LoginView.vue";
 import MenuView from "@/views/MenuView.vue";
 import BookingView from "@/views/BookingView.vue";
 import AboutView from "../views/AboutView.vue";
 import ContactView from "@/views/ContactView.vue";
+import LoginView from "@/views/account/LoginView.vue";
+import RegisterView from "@/views/account/RegisterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,11 +14,6 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: LoginView
     },
     {
       path: "/menu",
@@ -30,14 +26,24 @@ const router = createRouter({
       component: BookingView
     },
     {
+      path: "/about",
+      name: "about",
+      component: AboutView
+    },
+    {
       path: "/contact",
       name: "contact",
       component: ContactView
     },
     {
-      path: "/about",
-      name: "about",
-      component: AboutView
+      path: "/login",
+      name: "login",
+      component: LoginView
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterView
     }
   ]
 });
