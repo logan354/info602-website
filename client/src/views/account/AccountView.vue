2020705f2566a8642d5749fa672dd46eb521103b
Logan <75645441+Block354@users.noreply.github.com>
2024-05-20 18:32:19 +1200

    Create AccountView.vue

---
+++ client/src/views/account/AccountView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from "../../components/Navigation.vue";
+import Footer from "../../components/Footer.vue";
+</script>
+
+<template>
+  <Navbar />
+  <div>Account View</div>
+  <Footer />
+</template>
+
+<style scoped></style>
+
+<script>
+</script>
