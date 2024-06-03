26bbe7719e4caa315a2b31d29a50e0ea3826539e
Logan <75645441+Block354@users.noreply.github.com>
2024-06-03 14:00:43 +1200

    fix(client): navigation profile

--- client/src/components/dashboard/DashboardSideNavigation.vue
+++ client/src/components/dashboard/DashboardSideNavigation.vue
@@ -6,7 +6,7 @@ import { RouterLink } from "vue-router";
   <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 250px; height: 750px">
     <ul class="nav nav-pills flex-column mb-auto" @click="handleClick">
       <hr>
-      <li class="nav-item">
+      <li>
         <a id="navigation-dashboard" class="nav-link text-white">
           <i class="bi bi-house-door-fill"></i>&nbsp;&nbsp;&nbsp;<RouterLink id="dashboard" class="router-link"
             to="/dashboard/home">Dashboard</RouterLink>
@@ -29,6 +29,10 @@ import { RouterLink } from "vue-router";
 </template>
 
 <style scoped>
+.nav li {
+  padding-bottom: 10px;
+}
+
 .router-link {
   text-decoration: none;
   color: white;
