2c302911f5cfc809a0268b97dcc49e1fd8d47c36
Raine Roberts <100312154+Ikatere@users.noreply.github.com>
2024-06-05 22:14:07 +1200

    feat(client): dashboard analytics (#25)
    * Update DashboardAnalytics.vue
    
    * Update DashboardAnalytics.vue
    
    changed src
    
    * fix(client): added spacing
    
    ---------
    
    Co-authored-by: Logan <75645441+Block354@users.noreply.github.com>

--- client/src/views/dashboard/DashboardAnalytics.vue
+++ client/src/views/dashboard/DashboardAnalytics.vue
@@ -11,8 +11,20 @@ import Footer from "../../components/Footer.vue";
             <DashboardSideNavigation />
         </div>
         <div class="content">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nesciunt a eum quas fuga modi magnam enim
-            est libero voluptates dolorum deleniti esse officiis, sunt porro voluptatum possimus maxime aliquam.
+            <h2 class="report-title">Analytics</h2>
+            <div class="controls">
+                <!-- Example control, add as needed -->
+                <label for="date-range">Date Range:&nbsp;</label>
+                <input type="date" id="start-date">&nbsp;
+                <input type="date" id="end-date">&nbsp;
+                <button @click="applyFilters">Apply</button>
+            </div>
+            <loading-spinner v-if="isLoading" />
+            <div class="iframe-wrapper" v-if="!isLoading">
+                <iframe title="Main_Connection_3"
+                    src="https://app.powerbi.com/reportEmbed?reportId=0384868a-6dcf-4d5d-b63d-d2d4688dadf8&autoAuth=true&ctid=f7bae8e1-a04b-4f86-9377-8eb84875e0a2"
+                    frameborder="0" allowFullScreen="false"></iframe>
+            </div>
         </div>
     </div>
     <Footer />
@@ -31,7 +43,58 @@ import Footer from "../../components/Footer.vue";
     flex-grow: 1;
     padding: 50px 100px 50px 100px;
 }
+
+.breadcrumb {
+    margin-bottom: 10px;
+    font-size: 14px;
+}
+
+.report-title {
+    margin-bottom: 10px;
+    font-size: 24px;
+    font-weight: bold;
+}
+
+.controls {
+    margin-bottom: 10px;
+}
+
+.iframe-wrapper {
+    width: 100%;
+    height: 0;
+    padding-bottom: 47.5%;
+    /* Adjust based on aspect ratio */
+    position: relative;
+    border: 3px solid #000;
+    /* 3px thick border */
+}
+
+iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
 </style>
 
 <script>
+export default {
+    data() {
+        return {
+            isLoading: true
+        };
+    },
+    mounted() {
+        // Simulate loading time
+        setTimeout(() => {
+            this.isLoading = false;
+        }, 2000);
+    },
+    methods: {
+        applyFilters() {
+            // Logic to apply filters to the Power BI report
+        }
+    }
+};
 </script>
