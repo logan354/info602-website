d98c4460a9392928c7a854628c5a672cd1a1a5a9
Logan <75645441+Block354@users.noreply.github.com>
2024-06-05 22:27:04 +1200

    feat(database): login

---
+++ database/Login.sql
@@ -0,0 +1,13 @@
+-- Create login
+CREATE LOGIN RasaiBE WITH PASSWORD = 'youshallnotpass';
+
+-- Add login to the sysadmin server role. This allows it to work with sequilize
+EXECUTE sp_addsrvrolemember 'RasaiBE', 'sysadmin';
+
+-- Use your desired database
+USE [YourDatabaseName];
+
+-- Add the login as a db_datareader and db_datawriter in the database
+CREATE USER RasaiBE FOR LOGIN RasaiBE;
+ALTER ROLE db_datareader ADD MEMBER RasaiBE;
+ALTER ROLE db_datawriter ADD MEMBER RasaiBE;
